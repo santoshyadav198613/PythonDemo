c61909599c7847fa6530d1425b54e9199e5856eb
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-09-14 21:07:01 +0530

    Added readlines method to read all lines

--- function.py
+++ function.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase=[]
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return  students_titlecase
 
 
@@ -18,8 +18,30 @@ def add_student(name, id=1):
     students.append(student)
 
 
+def save_file(student):
+    try:
+        f=open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+
+def read_file():
+    try:
+        f= open("students.txt","r")
+        for student in f.readlines():
+                add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file");
+
+
+read_file()
+print_students_titlecase()
+
 student_name = input("Enter Student Name:")
 student_id = input("Enter Student Id:")
 
 add_student(student_name, student_id)
-print_students_titlecase()
+save_file(student_name)
