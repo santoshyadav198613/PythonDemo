f623fbe7ea8654f951015cf258a897034a988f04
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-09-19 18:05:46 +0530

    Added classes and inheritance

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@ students = []
 
 class Student:
     school_name = "Test"
+
     def __init__(self, name, id=1):  # constructor
         student = {"name": name, "id": id}
         self.name = name
@@ -18,6 +19,18 @@ class Student:
     def get_school_name(self):
         return self.school_name
 
+
 santosh = Student("Santosh", 5)
 print(santosh.name)
 print(Student.school_name)
+
+
+class HighStudent(Student):  #inheritance
+    school_name = "High School";
+
+    def get_name_caps(self):  #Overloading
+        school_hsname = super().get_name_caps();
+        return  school_hsname+  "-HS"
+
+sam= HighStudent("Sam")
+print(sam.get_name_caps())
